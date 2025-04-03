130fb3732d1c422b8c835dfb62428c86d44c7d47
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-04-03 11:24:32 +0700

    validation nang cao

--- src/main/java/com/devteria/identify_service/Exception/ErrorCode.java
+++ src/main/java/com/devteria/identify_service/Exception/ErrorCode.java
@@ -10,12 +10,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized Error.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1001,"Invalid message key.", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1002, "User already exists.", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters.", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters.", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least {min} characters.", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password must be at least {min} characters.", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "User not exists.", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth.", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1008, "Your age must be at least {min}.", HttpStatus.BAD_REQUEST),
     ;
     private int code;
     private String message;
--- src/main/java/com/devteria/identify_service/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/devteria/identify_service/Exception/GlobalExceptionHandler.java
@@ -2,19 +2,26 @@ package com.devteria.identify_service.Exception;
 
 
 import com.devteria.identify_service.dto.request.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    private static final String MIN_ATTRIBUTE = "min";
 
     // define các loại exception mà mình bắt ở đây
     @ExceptionHandler(value = Exception.class)
-    ResponseEntity<ApiResponse> handingRuntimeException(RuntimeException ex){
+     ResponseEntity<ApiResponse> handingRuntimeException(RuntimeException ex){
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
         apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
@@ -45,17 +52,37 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<ApiResponse> handingMethodArgumentNotValidException(MethodArgumentNotValidException ex){
         String enumkey = ex.getFieldError().getDefaultMessage();
+
+        Map<String, Object> attributes = null;
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
         try {
             errorCode = ErrorCode.valueOf(enumkey);
+
+            // lấy attribute mà ta đã truyền vào annotation
+            //
+            var constraintViolations = ex.getBindingResult()
+                    .getAllErrors()
+                    .get(0)
+                    .unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolations.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
         }catch (IllegalArgumentException e){
 
         }
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(
+                Objects.nonNull(attributes)
+                        ? mapAttribute(errorCode.getMessage(), attributes)
+                        : errorCode.getMessage());
+
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
 
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
+    }
 }
